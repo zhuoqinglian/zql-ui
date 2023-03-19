14c4364f64a8cc1ad6062d8b94e9cb57dbbee003
objectjq <1035449612@qq.com>
2023-03-19 17:08:18 +0800

    chore: eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-  extends: require.resolve('@umijs/lint/dist/config/eslint'),
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'xo'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		quotes: ['error', 'single'],
+		'no-trailing-spaces': ['error'],
+		'object-curly-spacing': ['error', 'always'],
+		'keyword-spacing': 'error',
+		'space-infix-ops': ['error', { int32Hint: false }],
+		'padding-line-between-statements': [
+			'error',
+			{ blankLine: 'always', prev: 'block-like', next: '*' },
+		],
+		'comma-spacing': ['error', { before: false, after: true }],
+		'no-multi-spaces': 'error',
+		'arrow-spacing': 'error',
+	},
 };
